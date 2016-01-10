aa8583b7abc3c5bafa89ae1df1544940c36c499b
slallement <serguei.l@orange.fr>
2016-01-10 01:55:54 +0100

    missing mission file from previous commit

---
+++ include/Config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+namespace Config {
+    constexpr char TITLE_WINDOW[] = "Full of particles";
+
+    constexpr unsigned int WINDOWS_WIDTH  = 800;
+    constexpr unsigned int WINDOWS_HEIGHT = 600;
+}
+
+#endif
